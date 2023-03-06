6e5dd45b26bd65abb8a76ad8a0c3cc84c36c67f9
collectivecloudperu <collectivecloudperu@gmail.com>
2023-03-05 22:30:51 -0500

    Cambios

---
+++ js/app.js
@@ -0,0 +1,32 @@
+function obtenerDatos() {
+
+  fetch("https://nubecolectiva.com/api/v1/postres").then(response => {
+      if (response.ok) {
+          return response.json();
+      }
+  }).then(data => {
+      if (data) {
+          console.log(data);
+          const ar = data.postres;
+          let result = `<table class='table table-striped table-bordered table-hover text-center'><thead>
+                          <tr class=table-dark>
+                            <th>Nombre</th>
+                            <th>Precio</th>
+                            <th>Stock</th>
+                            <th>Imagen</th>
+                          </tr>
+                          </thead><tbody>`;
+          ar.forEach((elem) => {
+              result += `<tr>
+                    <td class=v-align-middle>${elem.nombre}</td>
+                    <td class=v-align-middle>${elem.precio}</td>
+                    <td class=v-align-middle>${elem.stock}</td>
+                    <td class=v-align-middle><img src=img/${elem.img} class=img-fluid style=max-width:30px></td>
+                  </tr>`
+          });
+          result += `</tbody></table>`;
+          document.getElementById("datos").innerHTML = result;
+      };
+  }).catch(err => console.error(err));
+
+}
